965e9a4deac2ef9cce57b89de7c192f455e41b19
Gabriel Feitosa <gabrielfcb13@gmail.com>
2023-12-07 15:57:18 -0300

    Adicionado arquivos do site de motos

---
+++ script.js
@@ -0,0 +1,31 @@
+// Barra de busca
+function redirect() {
+  // Obtém o valor digitado na caixa de pesquisa
+  let digitado = $('#searchInput').val().toLowerCase();
+
+  // Verifica o valor e redireciona para a página correspondente
+  switch (digitado) {
+    case 'costum':
+      window.location.href = 'costum.html';
+      break;
+    case 'motos':
+    case 'moto':
+      window.location.href = 'index.html';
+      break;
+    case 'driver':
+    case 'drivers':
+      window.location.href = 'driver.html';
+      break;
+    case 'vespas':
+    case 'vespa':
+      window.location.href = 'vespas.html';
+      break;
+    default:
+      // Redireciona para uma página padrão se nenhum termo correspondente for encontrado
+      alert("Página não encontrada!");
+      alert(" Busque por nossas motos Costum, Driver ou Vespas");
+      break;
+  }
+}
+
+
